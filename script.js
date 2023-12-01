42d794014c065876917ddc4e98a10b2aa61d237a
Durgam Karthik <104593984+Karthik1224@users.noreply.github.com>
2023-12-01 19:47:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 //your JS code here. If required.
- var openModal = document.getElementById("openModal");
-           var closeModal = document.getElementsByClassName("close-modal")[0];
-           var modal = document.getElementsByClassName("modal")[0];
-           openModal.onclick = function()
-           {
-              modal.style.display = "block";
-           }
-           closeModal.onclick = function()
-           {
-              modal.style.display = "none";
-           }
